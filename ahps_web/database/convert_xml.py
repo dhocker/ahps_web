379e340caaea9b25181e2dbd90fbeccd64363d4e
Dave Hocker <dghocker@charter.net>
2014-10-04 21:28:13 -0400

    Start work on xml conversion program

---
+++ ahps_web/database/convert_xml.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python
+
+#
+# AHPS Web - web server for managing an AtHomePowerlineServer instance
+# Copyright (C) 2014  Dave Hocker (email: AtHomeX10@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the LICENSE file for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program (the LICENSE file).  If not, see <http://www.gnu.org/licenses/>.
+#
+# To convert an existing AtHomeX10 XML file:
+#   workon flask            # Establish working virtual environment with Flask
+#   python convert_xml.py name.xml
+
+from xml.dom.minidom import parse
+import xml.dom.minidom
+import sys
+import io
+import os
+from sqlite3 import dbapi2 as sqlite3
+from init_db import init_db
+
+
+def create_db():
+    """
+    Opens the conversion database.
+    Always starts with a new, empty database by deleting any existing one.
+    """
+    db_name = "ahps_web.sqlite3"
+
+    # TODO Check for DB existence. If it does not exist, create a new
+    # DB. If it does exist, just open the existing DB file. This
+    # way we can incrementally add existing house files to the DB.
+
+    try:
+        os.remove(db_name)
+    except:
+        pass
+
+    init_db()
+
+    rv = sqlite3.connect(db_name)
+    rv.row_factory = sqlite3.Row
+
+    return rv
+
+
+def convert_program(program, db):
+    days = program.getAttribute("days")
+    print "    ", days
+
+    # insert program for module
+
+
+def convert_device(device, db):
+    device_name = device.getAttribute("name")
+    print "  ", device_name
+
+    # insert device as module
+    # get moduleid
+
+    programs = device.getElementsByTagName("program")
+    for program in programs:
+        convert_program(program, db)
+
+
+def convert_room(room, db):
+    room_name = room.getAttribute("name")
+    print room_name
+
+    # insert room
+    # get roomid
+
+    devices = room.getElementsByTagName("device")
+    for device in devices:
+        convert_device(device, db)
+
+
+#
+# main
+#
+if __name__ == "__main__":
+    xml_file = sys.argv[1]
+    dom = xml.dom.minidom.parse(xml_file)
+
+    db = create_db()
+
+    house = dom.documentElement
+
+    # Insert house
+    # get houseid
+
+    # Get all of the rooms in the house document element
+    rooms = house.getElementsByTagName("room")
+    for room in rooms:
+        convert_room(room, db)
+
+    db.close()
+
+
+# <room name="Craft Room">
+#   <device module-type="appliance" name="Light, Wreath, Candles" house-code="B" device-code="5" x="8" y="7">
+#     <program days="MTWTFSS" start-trigger-method="sunset" start-time="7:15 PM" start-sunset-offset="-20"
+#       start-sunrise-offset="0" stop-trigger-method="clock-time" stop-time="10:05 PM" stop-sunset-offset="0"
+#       stop-sunrise-offset="0" start-randomize="0" stop-randomize="0" start-randomize-amount="0"
+#       stop-randomize-amount="0" start-action="on" stop-action="off" start-dim-percent="0" stop-dim-percent="0" />
+#   </device>
+# </room>
