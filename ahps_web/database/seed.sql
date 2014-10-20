075d42a738cf0a6450b366499c70af5e937d82e0
Dave Hocker <AtHomeX10@gmail.com>
2014-10-20 09:54:29 -0400

    Add house to database seed data

--- ahps_web/database/seed.sql
+++ ahps_web/database/seed.sql
@@ -1,9 +1,11 @@
-insert into rooms (name, description) values ('Family Room', 'Family room');
+insert into houses (name, current) values ('Test House', 1);
+
+insert into rooms (houseid, name, description) values (1, 'Family Room', 'Family room');
 
 insert into modules (roomid, module_type, name, house_code, device_code) values (1, 'appliance', 'Fireplace Left', 'a', '1');
 insert into modules (roomid, module_type, name, house_code, device_code) values (1, 'appliance', 'Fireplace Right', 'a', '2');
 
-insert into rooms (name, description) values ('Studio', 'Dave''s office and studio');
+insert into rooms (houseid, name, description) values (1, 'Studio', 'Dave''s office and studio');
 
 insert into modules (roomid, module_type, name, house_code, device_code) values (2, 'appliance', 'Harvest Tree', 'A', '7');
 insert into modules (roomid, module_type, name, house_code, device_code) values (2, 'lamp', 'Corner Light', 'L', '1');
