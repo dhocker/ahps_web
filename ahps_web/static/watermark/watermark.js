5fe43fa6a35b24e7fe234dca5e53ce3971b34003
Dave Hocker <dghocker@charter.net>
2014-10-21 15:25:19 -0400

    Fix change tracking false positive on add room page. Implement textbox watermark jQuery plugin. Apply watermark to home page. Apply watermark to program page.

---
+++ ahps_web/static/watermark/watermark.js
@@ -0,0 +1,41 @@
+/*
+    Watermark plugin
+    By Mitch Satchwell October 29th, 2012
+    http://mitchsatchwell.com/jquery-watermark-plugin/
+
+    Modifications by Dave Hocker October 21, 2014
+    AtHomeX10@gmail.com
+*/
+
+(function($){
+$.fn.watermark = function(options) {
+    var settings = $.extend({
+        'text': 'watermark',
+        'color': '#c0c0c0',
+        'font_style': 'italic',
+        }, options);
+    var original_color = $(this).css('color');
+    var original_font_style = $(this).css('font-style');
+
+    $(this).focus(function() {
+        if ($(this).val() == settings.text) {
+            $(this).val('');
+            $(this).css('color', original_color);
+            $(this).css('font-style', original_font_style);
+        }
+    });
+
+    $(this).blur(function() {
+        if($(this).val().length == 0) {
+            $(this).val(settings.text);
+            $(this).css('color', settings.color);
+            $(this).css('font-style', settings.font_style);
+        }
+    });
+
+    // Initialize the element
+    this.blur();
+
+    return this;
+};
+}(jQuery));
